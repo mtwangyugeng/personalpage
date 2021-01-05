e2ec8e7d28916a719df5f52b747b3482cd7828e2
Simon Dickenson <mtwangyugeng@gmail.com>
2021-01-05 16:28:53 -0500

    completed name card

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import NameCard from './namecard/NameCard'
 
 ReactDOM.render(
   <React.StrictMode>
-    <NameCard/>
+    <NameCard login = "mtwangyugeng"/>
     <ConnectionBar 
       linkedin = "https://www.linkedin.com/in/yu-geng-wang-474553192/"
       github = "https://github.com/mtwangyugeng"
     />
     <SlidePanels contents = {[-1,0, <iframe width="100%" height="100%" src="https://mtwangyugeng.github.io/hbeatbox/" title="W3Schools Free Online Web Tutorials"></iframe>, 2, 3, 4]} />
-    <App login = "mtwangyugeng"/>
+    {/* <App login = "mtwangyugeng"/> */}
     {/* <iframe src="https://mtwangyugeng.github.io/hbeatbox/" title="W3Schools Free Online Web Tutorials"></iframe>  */}
   </React.StrictMode>,
   document.getElementById('root')
--- src/namecard/NameCard.js
+++ src/namecard/NameCard.js
@@ -4,17 +4,54 @@ import './NameCard.css';
 export default class NameCard extends React.Component{
     /**
      * As a NameCard, I:
-     *  Displayer basic infomation based on
-     *  the github login given
+     *  - Display basic infomation based on
+     *      the github login given
      * I support: 
      *  -this.props.login
      */
+
+    state = {
+        loading: true,
+        error: null, 
+
+    }
+    data = null
+
     render() {
         return ( 
             <div className = "NameCard-container">
-                
+                {this.render_putin()}
             </div>
         );
     }
+
+    componentDidMount() {
+        if(!this.props.login) return;
+        this.setState({loading: true});
+        fetch(`https://api.github.com/users/${this.props.login}`)
+            .then((response) => response.json())
+            .then((jsondata) => {this.data = jsondata})
+            .then(() => this.setState({loading: false}))
+            .catch((e) => this.setState({error: e}));
+
+    }
+
+    render_putin = () => {
+        if(this.state.loading) return <h1>Loading...</h1>;
+        if(this.state.error) return <pre>{JSON.stringify(this.state.error, null, 2)}</pre>;
+        if(!this.data) return null;
+        return (
+            <>
+                <img className = "profile-pic" alt = {this.data.login} src = {this.data.avatar_url} />
+                <div className = "profile-info">
+                    <h2>{this.data.name}</h2>
+                    <p>{this.data.login}</p>
+                    <p>{this.data.location}</p>
+                    <div className = "profile-info-bio">{this.data.bio}</div>
+                </div>
+            </>
+        )
+    }
+
 }
 
