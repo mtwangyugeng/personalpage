8bded1de2c6ef94dca8a6b4285cfc79ca39ce498
Simon Dickenson <mtwangyugeng@gmail.com>
2021-01-04 23:34:04 -0500

    sticky bar in react??? wooot

--- src/connectionbar/ConnectionBar.js
+++ src/connectionbar/ConnectionBar.js
@@ -11,13 +11,34 @@ export default class SpeedChanger extends React.Component{
      *  -this.props.linkedin
      *  -this.props.github
      */
+    state = {
+        stick: ""
+    }
+
     render() {
         return ( 
-            <div className = "the-bar">
-                <div className = "linkedin icons" onClick = {this.golinkedin}></div>
+            <div id = "sticktop" className = {"the-bar "  + this.state.stick}>
+                <div className = {"linkedin icons"} onClick = {this.golinkedin}></div>
                 <div className = "github icons" onClick = {this.gogithub}></div>
             </div>
         );
+    }
+
+    componentDidMount(){
+        var header = document.getElementById("sticktop");
+        this.sticky = header.offsetTop;
+        window.onscroll = this.stick_it;
+    }
+
+    stick_it = () =>{
+        if(window.pageYOffset > this.sticky) 
+            this.setState({
+                stick: "sticky"
+            })
+        else
+            this.setState({
+                stick: ""
+            })
     }
 
     golinkedin = () => {
