00653637b7ce74169a319bd5bb9d01cbc00fd68d
Simon Dickenson <mtwangyugeng@gmail.com>
2021-01-20 21:52:45 -0500

    added some new experiences

--- src/contentcontainer/ContentContainer.js
+++ src/contentcontainer/ContentContainer.js
@@ -35,24 +35,39 @@ export default class ContentContainer extends React.Component{
         imsrc: "https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Utoronto_coa.svg/1200px-Utoronto_coa.svg.png",
         title: "Agile Group Projects",
         tags: ["Academic","Agile", "Scrums"], 
-        time: "2017-2019",
-        description: "Agile Development project experience from CSCC01, CSCC10, CSCD01"
+        time: "2017 - 2019",
+        description: "Agile Development project experience from CSCC01, CSCD01"
     }
     ec2 = {
         imsrc: "https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Utoronto_coa.svg/1200px-Utoronto_coa.svg.png",
         title: "Operating System Mechanisms",
         tags: ["Academic","C","Shell"], 
-        time: "Apr 2018- Aug 2018",
+        time: "Apr 2018 - Aug 2018",
         description: "Reproduced OS mechanics using C in CSCC69"
     }
     ec3 = {
+        imsrc: "https://2.bp.blogspot.com/-tzm1twY_ENM/XlCRuI0ZkRI/AAAAAAAAOso/BmNOUANXWxwc5vwslNw3WpjrDlgs9PuwQCLcBGAsYHQ/s1600/pasted%2Bimage%2B0.png",
+        title: "Phone App Development",
+        tags: ["Academic","Andriod Studio","java"], 
+        time: "Jan 2016 - Mar 2016",
+        description: "Developed a plane ticket booking app."
+    }
+    ec4 = {
+        imsrc: "https://upload.wikimedia.org/wikipedia/commons/e/ed/Toad-Original_RGB.png",
+        title: "UI Development",
+        tags: ["Academic", "Toad for Oracle", "HCI"], 
+        time: "Apr 2018 - Aug 2018",
+        description: "Developed a new UI fot TforO."
+    }
+    ecl = {
         imsrc: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
         title: "Web Development",
         tags: ["Self-learning","React.js"], 
-        time: "Dec 2020- Jan 2021",
-        description: "Reproduced OS mechanics using C in CSCC69"
+        time: "Dec 2020 - Jan 2021",
+        description: "Self-Learning Javascript, CSS, HTML, React.js."
     }
-    ecs = [this.ec1, this.ec2, this.ec3]
+    
+    ecs = [this.ec1, this.ec2, this.ec3, this.ec4, this.ecl]
     sec2 = {
         title: "Experience",
         content: <div style = {{display: "flex",justifyContent:"space-around", flexWrap: "wrap",width: "100%", backgroundColor:"black", padding:"0.5vmin"}}>
